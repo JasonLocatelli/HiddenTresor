a6c160c74c2ea56bbc1396edfd57801683ec0469
neven98 <caseyb@hotmail.fr>
2024-08-11 19:17:46 +0200

    Fonctionnement movement in and out water

--- Scripts/water_body.gd
+++ Scripts/water_body.gd
@@ -3,48 +3,49 @@ extends Node2D
 @export var k = 0.015
 @export var d = 0.03
 @export var spread = 0.0003
-
-var springs = []
-var passed = 8
-
 @export var distance_between_springs = 32
 @export var spring_number = 6
 @export var border_thickness = 1.1
+@export var depth = 1000
 
 @onready var water_spring = preload("res://Scenes/water_spring.tscn")
 @onready var water_polygon = $WaterPolygon
 @onready var water_border = $WaterBorder
 
-@export var depth = 1000
+var springs = []
+var passed = 8
 var target_height = global_position.y
 var bottom = target_height + depth
 
-
-
 func _ready():
 	water_border.width = border_thickness
-	
 	for i in range(spring_number):
 		var x_position = distance_between_springs * i
-		var w = water_spring.instantiate()
-		add_child(w)
-		springs.append(w)
-		w.initialize(x_position, i)
-		w.set_collision_width(distance_between_springs)
-		w.connect("splash",splash)
+		var instance = water_spring.instantiate()
+		add_child(instance)
+		springs.append(instance)
+		instance.initialize(x_position, i)
+		instance.set_collision_width(distance_between_springs)
+		instance.connect("splash",splash)
+		
+	var total_lenght = distance_between_springs * (spring_number - 1)
+	var rectangle = RectangleShape2D.new().duplicate()
+	var rect_position = Vector2(total_lenght /2,depth /2)
+	var rect_extends = Vector2(total_lenght ,depth )
+	
+	$Water_body_area.position = rect_position
+	rectangle.size = rect_extends
+	$Water_body_area/water_body_collision.set_shape(rectangle)
 
 func _physics_process(delta):
 	for i in springs:
 		i.water_update(k,d)
-		
 	var left_deltas = []
 	var right_deltas = []
-	
 	for i in range (springs.size()):
 		left_deltas.append(0)
 		right_deltas.append(0)
 		pass
-		
 	for j in range(passed):
 		for i in range(springs.size()):
 			if i > 0:
@@ -68,14 +69,11 @@ func draw_water_body():
 	var curve = water_border.curve 
 	var points = Array(curve.get_baked_points())
 	var water_polygon_points = points
-	
 	var first_index = 0
 	var last_index = water_polygon_points.size()-1
-
 	water_polygon_points.append(Vector2(water_polygon_points[last_index].x, bottom))
 	water_polygon_points.append(Vector2(water_polygon_points[first_index].x, bottom))
 	water_polygon_points = PackedVector2Array(water_polygon_points)
-	
 	water_polygon.polygon = water_polygon_points
 	
 func new_border():
@@ -83,10 +81,16 @@ func new_border():
 	var surface_points = []
 	for i in range(springs.size()):
 		surface_points.append(springs[i].position)
-		
 	for i in range(surface_points.size()):
 		curve.add_point(surface_points[i])
-		
 	water_border.curve = curve
 	water_border.smooth(true)
 	water_border.queue_redraw()
+
+func _on_water_body_area_body_entered(body):
+	if (body.is_in_group("player")) :
+		body.max_speed = body.max_speed/2
+
+func _on_water_body_area_body_exited(body):
+	if (body.is_in_group("player")) :
+		body.max_speed = body.max_speed*2
--- Scripts/water_spring.gd
+++ Scripts/water_spring.gd
@@ -1,19 +1,17 @@
 extends Node2D
 
-var velocity = 0
-
-var force = 0
-
-var height = 0
-
-var target_height = 0
-
 @onready var collision = $Area2D/CollisionShape2D
+
+var velocity = 0
+var force = 0
+var height = 0
+var target_height = 0
 var index = 0
 var motion_factor = 0.014
 
 var collided_with = null
 signal splash
+
 func water_update(sprintConstant, dampening):
 	height = position.y
 	var x = height - target_height
@@ -34,13 +32,10 @@ func set_collision_width(value):
 	var new_extents = Vector2(value/2, extents.y)
 	collision.shape.size = new_extents
 
-
 func _on_area_2d_body_entered(body):
-	
 	if body == collided_with:
 		return
 	collided_with = body
-	
-	var speed = body.velocity.y * motion_factor
-	body.max_speed -= 90
-	emit_signal("splash", index, speed)
+	if (!(body is StaticBody2D)) :
+		var speed = body.velocity.y * motion_factor
+		emit_signal("splash", index, speed)
