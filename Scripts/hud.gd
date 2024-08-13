f6725c63008f6237dedd9bd51f674d24014ee082
JSkey <jskeydeveloper@gmail.com>
2024-08-13 15:39:34 +0200

    game over

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -2,12 +2,11 @@ extends Control
 class_name HUD
 
 var progressbarOxygen : ProgressBar
+var animGameOver : AnimationPlayer
 
 func _ready():
 	progressbarOxygen = $PbOxygen
-
-func _process(delta):
-	pass
+	animGameOver = $GameOver/AnimationPlayer
 
 func updateValuePbOxygen(value):
 	progressbarOxygen.value = value
@@ -15,3 +14,14 @@ func updateValuePbOxygen(value):
 func updateMaxValuePbOxygen(value : float):
 	progressbarOxygen.max_value = value
 	
+# Méthode chargée d'activer l'animation pour afficher le game over.
+func displayGameOver():
+	animGameOver.play("displayGameOver")
+
+# Méthode chargée s'assigner un evènement au bouton "Restart".
+func _on_btn_restart_pressed():
+	GameManager.load_game_scene()
+
+# Méthode chargée d'assigner un evènement au bouton "Quit".
+func _on_btn_quit_pressed():
+	print("Display main menu")
