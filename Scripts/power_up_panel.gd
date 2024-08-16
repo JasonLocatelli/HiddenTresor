174d5e4e559f014c155aecac4f3a5cc785e53778
JSkey <jskeydeveloper@gmail.com>
2024-08-16 22:42:46 +0200

    fix-ui-shop

--- Scripts/power_up_panel.gd
+++ Scripts/power_up_panel.gd
@@ -1,10 +1,10 @@
-extends ColorRect
+extends PanelContainer
 
 var item = null
 
 func initElement() :
-	#$ColorRect/Sprite2D.texture = item["icon"]
-	$name.text = item["displayname"]
-	$description.text = item["details"]
-	$price.text = str(item["priceInital"])
+	#$HbcIcon/ArcIcon/TrIcon.texture = item["icon"]
+	$HbcIcon/McNameDesc/HbcNameDesc/name.text = item["displayname"]
+	$HbcIcon/McNameDesc/HbcNameDesc/description.text = item["details"]
+	$HbcIcon/McPriceBtn/VbcPriceBtn/HbcPriceBtn/price.text = str(item["priceInital"])
 	#$ColorRect/Sprite2D.scale = Vector2($ColorRect/Sprite2D.texture.get_width(), $ColorRect/Sprite2D.texture.get_height())
